76f4b9388c70e07029d48e9c09bd71b7b1a9119d
0-haha <0-haha@lalala.org>
2020-06-18 21:11:52 -0400

    Add script to generate random key

---
+++ src/lib/test/genKey.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"template2/lib/util"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: genKey.go [keyLength]")
+		os.Exit(0)
+	}
+	keyLength, err := strconv.Atoi(os.Args[1])
+
+	if err != nil || keyLength <= 0 {
+		fmt.Println("keyLength must be an positiveinteger")
+		os.Exit(0)
+	}
+	value, _ := util.GenerateRandomKey(keyLength)
+	fmt.Println(util.Base64Encode(value))
+}
