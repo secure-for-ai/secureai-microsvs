237a06c8bfb2a215d15336e9e36ae393e3636a8d
0-haha <0-haha@lalala.org>
2020-07-08 10:58:48 -0400

    create database and index with case-insensitive and unique key

--- replica-init.js
+++ replica-init.js
@@ -1,2 +1,3 @@
 db.createUser({user:"test", pwd: "password",  roles: [{role: "readWrite", db: "gtest" }], mechanisms:["SCRAM-SHA-1"],passwordDigestor:"server" })
-db.createCollection("user")
+db.createCollection("user", { collation: { locale: 'en_US', strength: 2 } } )
+db.user.createIndex( { "username": 1 }, { unique: true, collation: { locale: 'en_US', strength: 2 } } )
