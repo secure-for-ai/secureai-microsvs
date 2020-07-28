84a4d1212e9d77710d45f0d6897c2beb94bcb538
0-haha <0-haha@lalala.org>
2020-07-28 01:19:01 -0400

    add aes gcm cipher in utility

---
+++ src/lib/test/cipher.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"template2/lib/util"
+)
+
+var (
+	key, _    = util.Base64Decode("pR6kDdHYqNMRO74kUxFiGgpv3A6qKFeCY6IDHxDH8NY")
+	cipher, _ = util.NewAesGcmCipher(key)
+	plainText = "hello world"
+)
+
+func main() {
+	fmt.Println("PlainText:", plainText)
+	cipherText, _ := cipher.Encrypt(plainText)
+	fmt.Println("CipherText:", cipherText)
+	decryptText, _ := cipher.Decrypt(cipherText)
+	fmt.Println("DecryptText:", decryptText)
+}
---
+++ src/lib/util/cipher.go
@@ -0,0 +1,80 @@
+package util
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"errors"
+)
+
+var ErrCipherText = errors.New("ErrCipherText")
+
+type AesGcmCipher struct {
+	ahead cipher.AEAD
+}
+
+func NewAesGcmCipher(encKey []byte) (*AesGcmCipher, error) {
+	c, err := aes.NewCipher(encKey)
+	if err != nil {
+		return nil, err
+	}
+	ahead, err := cipher.NewGCMWithNonceSize(c, 12)
+	if err != nil {
+		return nil, err
+	}
+	return &AesGcmCipher{
+		ahead: ahead,
+	}, nil
+}
+
+func (c AesGcmCipher) Encrypt(str string) (string, error) {
+
+	strByte := []byte(str)
+
+	iv, err := GenerateRandomKey(12)
+
+	if err != nil {
+		// Todo add logger
+		return "", err
+	}
+
+	cipherText := c.ahead.Seal(nil, iv, strByte, nil)
+	output := make([]byte, 12+len(cipherText))
+
+	copy(output, iv)
+	copy(output[12:], cipherText)
+
+	return Base64Encode(output), nil
+}
+
+func (c AesGcmCipher) Decrypt(str string) (string, error) {
+	ciperText, err := Base64Decode(str)
+
+	if err != nil {
+		return "", err
+	}
+
+	// check cipherText format
+	// cipherText must contain 12 byte initial vector + 16 byte tag
+	// Todo dynamic configurable initial vector size + tag size
+	if len(ciperText) < 28 {
+		return "", ErrCipherText
+	}
+
+	plainText, err := c.ahead.Open(nil, ciperText[0:12], ciperText[12:], nil)
+
+	if err != nil {
+		return "", err
+	}
+
+	plainTextLen := len(plainText)
+
+	// check plainText format
+	// plaintext contains 8 bytes timestamp plus random session ID
+	if plainTextLen < 9 {
+		return "", ErrCipherText
+	}
+
+	output := string(plainText)
+
+	return output, nil
+}
